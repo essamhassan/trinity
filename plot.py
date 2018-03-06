5ace5381770cc2704417f47566ac5a5fd062a1fa
Essam A. Hassan <es.hassan187@gmail.com>
2018-03-06 06:13:46 +0200

    add spread factor ax

--- plot.py
+++ plot.py
@@ -5,17 +5,22 @@ import matplotlib.dates as mdates
 import numpy as np
 
 
-def graphRaw():
-    date, bid, ask = np.loadtxt('GBPUSD1d.txt', unpack=True,
+def graphRaw(path):
+    date, bid, ask = np.loadtxt(path, unpack=True,
             delimiter = ',', converters={0:mdates.strpdate2num('%Y%m%d%H%M%S')})
     fig = plt.figure(figsize=(10,7))
     axl = plt.subplot2grid((40,40),(0,0), rowspan=40, colspan=40)
     axl.plot(date, bid)
     axl.plot(date, ask)
+    plt.gca().get_yaxis().get_major_formatter().set_useOffset(False)
 
     axl.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
     for label in axl.xaxis.get_ticklabels():
         label.set_rotation(45)
-    plt.gca().get_yaxis().get_major_formatter().set_useOffset(False)
+
+    axl_2 = axl.twinx()
+    axl_2.fill_between(date, 0, (ask-bid), facecolor='g', alpha=.3)
+    plt.subplots_adjust(bottom=.23)
+
     plt.grid(True)
     plt.show()
